0389391d30b110ed2fa44445c4b6cedbb310180f
nomarion <macmini@MBP-Igor.netis.cc>
2025-06-30 20:20:01 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let company = {
+    sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600}],
+    development: {
+        web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800}],
+        internals: [{name: 'Jack', salary: 1300}],
+    }
+}
+
+const handler = (object) => {
+    let result = 0;
+    const run = (object) => {
+        if (Array.isArray(object)) {
+            for (let item of object) {
+                result += item.salary ?? 0;
+            }
+        } else {
+            for (let key in object) {
+                run(object[key]);
+            }
+        }
+    }
+    run(object);
+    return result;
+}
+
+console.log(handler(company));
+
